2b8b88cb835597021377763880964c38f001cb1a
solidest <solidest@163.com>
2019-11-23 23:10:45 +0800

    update

--- demo/case/case1.lua
+++ demo/case/case1.lua
@@ -1,7 +1,8 @@
 
-function main()
-    print("Hello world!")
+require "string"
 
+function test_print()
+    -- print
     for key,value in pairs(cit)
     do
     print("Interface: ", key)
@@ -11,15 +12,76 @@ function main()
     do
     print("Frame: ", key)
     end
+end
 
+function test_write()
+    --write
     write(cit['do'], true)
     print("writed do 1")
     write(cit['do'], false)
     print("writed do 2")
     write(cit.ad, 100)
     print("wirted ad")
+end
+
+function test_sendBuffer()
+    --sendBuffer
+    local str = string.format("test %d", 100)
+    sendBuffer(cit.serial, str);
+    print("sendBuffer serial")
+
+end
+
+function test_send()
+    --send
+    local gps_data = { ["经度信息"]=99.888, ["纬度信息"]=777.678}
+    send(cit['232'], cpt.pr_GPS, gps_data, {a=true});
+end
+
+function test_recv()
+    local v, o = recv(cit['232'], cpt.pr_GPS, 3000);
+    print("recv prot v: ", v["经度信息"], ", ", v["纬度信息"], "  o: ", o)
+end
+
+function test_delay()
+    --delay
+    print("before delay: ", now())
+    delay(500);
+    print("after delay: ", now())
+end
+
+function test_assert()
+    --assert
+    assert.ok(true);
+    assert.ok(false, "eeeeeee");
+end
+
+function test_read()
+    --recv
+    print("read di value: ", read(cit.di))
+    print("read da value: ", read(cit.da))
+    v, o = recvBuffer(cit.serial)
+    print("recv serial buffer1:  string:", v, "  option:", o)
+    print("recv serial buffer2:  ", recvBuffer(cit.serial))
+end
+
+function test_other()
     print("MaxStep: ", maxStepTick())
-    --assert.ok(false, "error1")
+end
+
+function main()
+    print("\n\nStart run case:")
+
+    test_print()
+    test_write()
+    test_sendBuffer()
+    test_send()
+    test_delay()
+    --test_assert()
+    test_read()
+    test_recv()
+    test_other()
+
 end
 
 main()
--- test/test_binding.js
+++ test/test_binding.js
@@ -7,17 +7,20 @@ function testBasic()
 {
     let srv = new SrvApi("127.0.0.1", 1210);
     let s = srv.state();
-    assert.ok(s.work_type=="idle");
+    assert.ok(s.work_type=="idle" || s.work_type=="prepared");
 
-    srv.prepare(1, "/home/pi/srv_api/demo/interface", "/home/pi/srv_api/demo/protocol");
-    s = srv.state();
-    assert.ok(s.work_type=="prepared");
+    if(s.work_type=="idle") {
+        srv.prepare(1, "/home/pi/srv_api/demo/interface", "/home/pi/srv_api/demo/protocol");
+        s = srv.state();
+        assert.ok(s.work_type=="prepared");        
+    }
+
 
     let script = "/home/pi/srv_api/demo/case/case1.lua";
     srv.start(1, script);
     //s = srv.state();
     //assert.ok(s.work_type=="running");
-    for(var t = Date.now();Date.now() - t <= 800;);
+    for(var t = Date.now(); Date.now() - t <= 1500;);
 
     // srv.pause(1, script);
     // s = srv.state();
@@ -32,9 +35,22 @@ function testBasic()
     // s = srv.state();
     // assert.ok(s.work_type=="prepared");
 
-    srv.clear(1);
+    i = Date.now()%2;
     s = srv.state();
-    assert.ok(s.work_type=="idle");
+    if(i==0 && s.work_type=="prepared") {
+        srv.clear(1);
+        s = srv.state();
+        assert.ok(s.work_type=="idle");
+    }
+    
+    for(;;) {
+        s = srv.state();
+        if(s.work_type=="running") {
+            continue;
+        } else {
+            break;
+        }
+    }
 
     //srv.exit();
 }
@@ -44,7 +60,13 @@ function testInvalidParams()
     const srv = new SrvApi();
 }
 
-testBasic();
+function testLoop() {
+    for(let i =0; i<1000; i++) {
+        testBasic();
+    }
+}
+
+testLoop();
 //assert.doesNotThrow(testBasic, undefined, "testBasic threw an expection");
 assert.throws(testInvalidParams, undefined, "testInvalidParams didn't throw");
 
